e3a5120f683c5d2d36f22772e415a0181ff1927d
igo <igoranatol870@gmail.com>
2022-06-28 08:32:57 +0300

    sdfsdf

---
+++ js/sig_or_regi.js
@@ -0,0 +1,108 @@
+function sig_or_regi () {
+    try {
+        if(confirm("Можете войти, и создатвать и изменять масси")) {
+            if(confirm("Чтобы создавать массивы, надо войт")) {
+                if(confirm("Войт")) {
+                    var us_use1 = {
+                        us_use1_use1: {
+                            us_use1_use1_nm1:"igo",
+                            us_use1_use1_sr1:"romanen",
+                            us_use1_use1_ag1:"19"
+                        }
+                    };
+                    if(us_use1.hasOwnProperty("us_use1_use1.us_use1_use1_nm1") == true && us_use1.hasOwnProperty("us_use1_use1.us_use1_use1_sr1") == true) {
+                        if(confirm("Введите им")) {
+                            var usi1_ch_nm1;
+                            var usi1_ch_sr1;
+                            var usi1_ch_ag1;
+                            usi1_ch_nm1 = prompt("Введите своё им");
+                            if(usi1_ch_nm1 != us_use1.us_use1_use1.us_use1_use1_nm1 && usi1_ch_nm1 != us_use1.us_use1_use1us_use1_use1_ag1) throw new Error("У вас ошибка, поле не может быть пуст");
+                            else if(usi1_ch_nm1 == us_use1.us_use1_use1.us_use1_use1_nm1 && usi1_ch_nm1 != us_use1.us_use1_use1us_use1_use1_ag1) {
+                                usi1_ch_sr1 = prompt("Введиет свою фамил");
+                                if(usi1_ch_sr1 != us_use1.us_use1_use1.us_use1_use1_sr1 && usi1_ch_sr1 != us_use1.us_use1_use1.us_use1_use1_sr1) throw new Error("У вас ошибка, поле не может быть пуст");
+                                else if(usi1_ch_sr1 == us_use1.us_use1_use1.us_use1_use1_sr1 && usi1_ch_sr1 != us_use1.us_use1_use1.us_use1_use1_nm1) {
+                                    usi1_ch_ag1 = prompt("Введиет свой возра");
+                                    if(usi1_ch_ag1 != us_use1.us_use1_use1.us_use1_use1_ag1 && usi1_ch_ag1 != us_use1.us_use1_use1.us_use1_use1_sr1) throw new Error("У вас ошибка, поле не может быть пуст");
+                                    else if(usi1_ch_ag1 == us_use1.us_use1_use1.us_use1_use1_ag1 && usi1_ch_ag1 != us_use1.us_use1_use1.us_use1_use1_sr1) {
+                                        if(confirm("Вы вошл")) {
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    catch(e) {
+        if(confirm("Вы можете зарегестриро")) {
+            if(confirm("Введите своё им")) {
+                var use_usi2_nm1;
+                var use_usi2_sr1;
+                var use_usi2_ag1;
+                use_usi2_nm1 = prompt("Введите своё им");
+                if(use_usi2_nm1 != us_use1.us_use1_use1.us_use1_use1_nm1 && use_usi2_nm1 != us_use1.us_use1_use1.us_use1_use1_sr1) {
+                    use_usi2_sr1 = prompt("Введите свою фамил");
+                    if(use_usi2_sr1 != us_use1.us_use1_use1.us_use1_use1_sr1 && use_usi2_sr1 != us_use1.us_use1_use1.us_use1_use1_ag1) {
+                        use_usi2_ag1 = prompt("Введите свой возра");
+                        if(use_usi2_ag1 != us_use1.us_use1_use1.us_use1_use1_ag1 && use_usi2_ag1 != us_use1.us_use1_use1.us_use1_use1_sr1) {
+                            if(confirm("Всё гото")) {
+                                if(confirm("Зарегестрировать i")) {
+                                    Object.defineProperty(us_use1, "us_use1_use2_nm1", {value:use_usi2_nm1, writable:true, enumerable:true, configurable:true});
+                                    if(us_use1.hasOwnProperty("us_use1_use2_nm1") == true && us_use1.us_use1_use2_nm1 != us_use1.us_use1_use1.us_use1_use1_nm1) {
+                                        Object.defineProperty(us_use1, "us_use1_use2_sr1", {value:use_usi2_sr1, writable:true, enumerable:true, configurable:true});
+                                        if(us_use1.hasOwnProperty("us_use1_use2_sr1") == true && us_use1.us_use1_use2_sr1 != us_use1.us_use1_use1.us_use1_use1_sr1) {
+                                            Object.defineProperty(us_use1, "us_use1_use2_ag1", {value:use_usi2_ag1, writable:true, enumerable:true, configurable:true});
+                                            if(us_use1.hasOwnProperty("us_use1_use2_ag1") == true && us_use1.us_use1_use2_ag1 != us_use1.us_use1_use1.us_use1_use1_ag1) {
+                                                if(confirm("Ваш id зарегестриро")) {
+                                                    if(confirm("Войт")) {
+                                                        if(confirm("Здравствуйт" + " " + us_use1.us_use1_use2_nm1 + " " + us_use1.us_use1_use2_sr1)) {
+                                                            if(confirm("Теперь вы можете посмотреть свой бала")) {
+                                                                if(confirm("Посмотре")) {
+                                                                    Object.defineProperty(us_use1, "us_use1_use2_pri1", {value:"1799", writable:true, enumerable:true, configurable:true});
+                                                                    if(us_use1.hasOwnProperty("us_use1_use2_pri1") == true && us_use1.us_use1_use2_pri1 != us_use1.us_use1_use1.us_use1_use1_ag1) {
+                                                                        if(confirm("Посмотреть свой бала")) {
+                                                                            if(confirm("Ваш бала" + " " + us_use1.us_use1_use2_pri1 + "$")) {
+                                                                                if(confirm("Можете сделать масс")) {
+                                                                                    if(confirm("Прежде чем сделать, надо что-то купи")) {
+                                                                                        if(confirm("Можете посмотреть список акци")) {
+                                                                                            if(confirm("Это платная функци")) {
+                                                                                                if(confirm("Посмотреть список акци")) {
+                                                                                                    var use_sel1 = {
+                                                                                                        use_sel1_us_1:"appl",
+                                                                                                        use_sel1_us_2:"samsun",
+                                                                                                        use_sel1_us_3:"xiaom",
+                                                                                                        use_sel1_us_4:"window",
+                                                                                                        use_sel1_us_5:"microso"
+                                                                                                    };
+                                                                                                    if(use_sel1.hasOwnProperty("use_sel1_us_1") == true && use_sel1.hasOwnProperty("use_sel1_us_3") == true) {
+                                                                                                        if(confirm("Список товаро")) {
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
